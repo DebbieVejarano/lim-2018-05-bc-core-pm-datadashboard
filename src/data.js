015951a311a58ac71f4184efe518806c2623e804
alexandra.vejarano@gmail.com <alexandra.vejarano@gmail.com>
2018-06-25 18:13:12 -0500

    Agregando propuesta de cambios a nueva rama

--- src/data.js
+++ src/data.js
@@ -7,12 +7,13 @@
 let userRaw = []
 let progressRaw
 let coursesRaw
+let campusesRaw
 
 //Variables globales
 let percent = new Object()
 let courses = []
 let usersWithStats = []
-//usersWithStats["stats"] = []
+usersWithStats['stats'] = []
 let filterNameArray = []
 
 //************************************/
@@ -72,10 +73,10 @@ let computeUsersStats = (users, progress, courses) => {
             unitsArray = Object.entries(progress[idUser][cohortName].units)
             unitsArray.map(units => {
               Object.entries(units[1].parts).map(unit => {
-                if (unit[1].type === "read") {
+                if (unit[1].type === 'read') {
                   numberRead++
                   completedRead += unit[1].completed
-                } else if (unit[1].type === "quiz") {
+                } else if (unit[1].type === 'quiz') {
                   numberQuiz++
                   completedQuiz += unit[1].completed
                   if (unit[1].completed === 0) {
@@ -83,7 +84,7 @@ let computeUsersStats = (users, progress, courses) => {
                   } else {
                     scoreSum += unit[1].score
                   }
-                } else if (unit[1].type === "practice") {
+                } else if (unit[1].type === 'practice') {
                   numberPractice++
                   completedPractice += unit[1].completed
                 }
@@ -99,28 +100,28 @@ let computeUsersStats = (users, progress, courses) => {
     percentQuiz = getPercent(completedQuiz, numberQuiz)
     percentPractice = getPercent(completedPractice, numberPractice)
 
-    userStats = {
+    userStats = {}
+    userStats.stats ={
       id: idUser,
       name: nameUser,
-      stats: {
-        percent: percentUser,
-        exercises: {
-          total: numberPractice,
-          completed: completedPractice,
-          percent: percentPractice
-        },
-        reads: {
-          total: numberRead,
-          completed: completedRead,
-          percent: percentRead
-        },
-        quizes: {
-          total: numberQuiz,
-          completed: completedQuiz,
-          percent: percentQuiz,
-          scoreSum: scoreSum,
-          scoreAvg: scoreAvg
-        }
+
+      percent: percentUser,
+      exercises: {
+        total: numberPractice,
+        completed: completedPractice,
+        percent: percentPractice
+      },
+      reads: {
+        total: numberRead,
+        completed: completedRead,
+        percent: percentRead
+      },
+      quizes: {
+        total: numberQuiz,
+        completed: completedQuiz,
+        percent: percentQuiz,
+        scoreSum: scoreSum,
+        scoreAvg: scoreAvg
       }
     }
 
@@ -131,7 +132,7 @@ let computeUsersStats = (users, progress, courses) => {
 }
 
 let sortUsers = (users, orderBy, orderDirection) => {
-  if (orderBy === "nombre" || orderBy === "name") {
+  if (orderBy === 'nombre' || orderBy === 'name') {
     users.stats.sort((a, b) => {
       x = a.name
       y = b.name
@@ -139,42 +140,42 @@ let sortUsers = (users, orderBy, orderDirection) => {
       return x < y ? -1 : x > y ? 1 : 0
     })
 
-    if (orderDirection === "DESC") {
+    if (orderDirection === 'DESC') {
       users.stats.reverse()
     }
   }
 
-  if (orderBy === "porcentaje" || orderBy === "percent") {
+  if (orderBy === 'porcentaje' || orderBy === 'percent') {
     users.stats.sort((a, b) => a.percent - b.percent)
-    if (orderDirection === "DESC") {
+    if (orderDirection === 'DESC') {
       users.stats.reverse()
     }
   }
 
-  if (orderBy === "pEjercicios" || orderBy === "pExercises") {
+  if (orderBy === 'pEjercicios' || orderBy === 'pExercises') {
     users.stats.sort((a, b) => a.exercises.percent - b.exercises.percent)
-    if (orderDirection === "DESC") {
+    if (orderDirection === 'DESC') {
       users.stats.reverse()
     }
   }
 
-  if (orderBy === "pQuizes" || orderBy === "pquizes") {
+  if (orderBy === 'pQuizes' || orderBy === 'pquizes') {
     users.stats.sort((a, b) => a.quizes.percent - b.quizes.percent)
-    if (orderDirection === "DESC") {
+    if (orderDirection === 'DESC') {
       users.stats.reverse()
     }
   }
 
-  if (orderBy === "pQuizesAvg" || orderBy === "pquizesavg") {
+  if (orderBy === 'pQuizesAvg' || orderBy === 'pquizesavg') {
     users.stats.sort((a, b) => a.quizes.scoreAvg - b.quizes.scoreAvg)
-    if (orderDirection === "DESC") {
+    if (orderDirection === 'DESC') {
       users.stats.reverse()
     }
   }
 
-  if (orderBy === "pLecturas" || orderBy === "pReads") {
+  if (orderBy === 'pLecturas' || orderBy === 'pReads') {
     users.stats.sort((a, b) => a.reads.percent - b.reads.percent)
-    if (orderDirection === "DESC") {
+    if (orderDirection === 'DESC') {
       users.stats.reverse()
     }
   }
@@ -186,7 +187,7 @@ let filterUsers = (users, search) => {
 
   users.stats.filter(
     x =>
-    x.name.includes(search.toUpperCase()) ? filterNameArray.stats.push(x) : 0
+      x.name.includes(search.toUpperCase()) ? filterNameArray.stats.push(x) : 0
   )
   return filterNameArray
 }
@@ -197,34 +198,41 @@ let processCohortData = options => {}
 // SOLO CUANDO TODAS LAS PROMESAS SE HAYAN CUMPLIDO
 //************************************/
 const dataUsers = fetch(
-  "../data/cohorts/lim-2018-03-pre-core-pw/users.json"
+  '../data/cohorts/lim-2018-03-pre-core-pw/users.json'
 ).then(response => response.json())
 const dataProgress = fetch(
-  "../data/cohorts/lim-2018-03-pre-core-pw/progress.json"
+  '../data/cohorts/lim-2018-03-pre-core-pw/progress.json'
 ).then(response => response.json())
-const dataCohorts = fetch("../data/cohorts.json").then(response =>
+const dataCohorts = fetch('https://api.laboratoria.la/cohorts').then(response =>
   response.json()
 )
+const dataCampuses = fetch('https://api.laboratoria.la/campuses').then(
+  response => response.json()
+)
 
-Promise.all([dataUsers, dataProgress, dataCohorts]).then(data => {
+Promise.all([dataUsers, dataProgress, dataCohorts, dataCampuses]).then(data => {
   //Copiamos la data de usuario en bruto
   userRaw = data[0]
   //Copiamos todo el progress en bruto
   progressRaw = data[1]
   //Copiamos todo los cursos en bruto
   coursesRaw = data[2]
+  //Copiamos los Campus
+  campusesRaw = data[3]
 
   //Procesamos la informacion recaudada en funciones
   computeCourses()
 
   //test
   computeUsersStats(userRaw, progressRaw, courses)
+  addButtonCampuses()
   //sortUsers(usersWithStats, "porcentaje", "ASC")
   //filterUsers(usersWithStats, "alexandra")
 })
-//agregar la api en el siguiente lado
 
-window.processCohortData = processCohortData
+
+
+/* window.processCohortData = processCohortData
 window.computeUsersStats = computeUsersStats
 window.sortUsers = sortUsers
-window.filterUsers = filterUsers
+window.filterUsers = filterUsers */
--- src/main.js
+++ src/main.js
@@ -1,99 +1,75 @@
-//Constantes
-const boxbody = document.getElementById('boxbody')
-const buttonStudents = document.querySelector('.buttonStudents')
+let mystyle = document.styleSheets[1]['cssRules']
+let acc = document.getElementsByClassName('accordion')
+let panel = document.querySelector('.panel')
+let cohortGrid = document.querySelector('.grid2x2')
 
-//Botón students
-buttonStudents.addEventListener('click', () => {
-  for (let i = 0; i < usersWithStats.length; i++) {
-    let divBody = document.createElement('div')
-    divBody.className = 'divBody'
-    boxbody.appendChild(divBody)
+//BOTONES ESTILO ACORDEON
+Object.keys(acc).map(x => {
+  acc[x].addEventListener('click', () => {
+    panel = acc[x].nextElementSibling
+    panelDisplay = panel.style.display
 
-    let boxname = document.createElement('div')
-    boxname.textContent = usersWithStats[i].name
-    boxname.innerHTML = 'nombre: ' + boxname.textContent
-    boxname.className = 'boxname'
-    divBody.appendChild(boxname) //pinto los users
-
-    
-    let boxid = document.createElement('div')
-    boxid.textContent = usersWithStats[i].id
-    boxid.innerHTML = 'id: ' + boxid.textContent
-    boxid.className= 'boxid'
-    divBody.appendChild(boxid) //pinto los IDs
-
-    let boxprogress = document.createElement('div')
-    boxprogress.textContent = usersWithStats[i].stats.percent
-    boxprogress.innerHTML = 'Progreso: ' + boxprogress.textContent + '%'
-    boxprogress.className = 'boxprogress'
-    divBody.appendChild(boxprogress) //pinto progress en pantalla
-//PRACTICE
-    let boxExercisesTotal = document.createElement('div')
-    boxExercisesTotal.textContent = usersWithStats[i].stats.exercises.total
-    boxExercisesTotal.innerHTML = 'Total de ejercicios: ' + boxExercisesTotal.textContent
-    boxExercisesTotal.className ='boxExercisesTotal'
-    divBody.appendChild(boxExercisesTotal) //pinto ejercicios totales en la pantalla
-
-    let boxExercisesCompleted = document.createElement('div')
-    boxExercisesCompleted.textContent = usersWithStats[i].stats.exercises.completed
-    boxExercisesCompleted.innerHTML = 'Ejercicios completados: ' + boxExercisesCompleted.textContent
-    boxExercisesCompleted.className ='boxExercisesCompleted'
-    divBody.appendChild(boxExercisesCompleted) //pinto ejercicios completados en la pantalla
-
-    let boxExercisesPercent = document.createElement('div')
-    boxExercisesPercent.textContent = usersWithStats[i].stats.exercises.percent
-    boxExercisesPercent.innerHTML = boxExercisesPercent.textContent + '%'
-    boxExercisesPercent.className ='boxExercisesPercent'
-    divBody.appendChild(boxExercisesPercent) //pinto % de ejercicios completados en la pantalla
-  //READ
-  let boxReadsTotal = document.createElement('div')
-  boxReadsTotal.textContent = usersWithStats[i].stats.reads.total
-  boxReadsTotal.innerHTML = 'Total de lecturas: ' + boxReadsTotal.textContent
-  boxReadsTotal.className ='boxReadsTotal'
-  divBody.appendChild(boxReadsTotal) //pinto reads totales en la pantalla
-
-  let boxReadsCompleted = document.createElement('div')
-  boxReadsCompleted.textContent = usersWithStats[i].stats.reads.completed
-  boxReadsCompleted.innerHTML = 'Lecturas completadas: ' + boxReadsCompleted.textContent
-  boxReadsCompleted.className ='boxReadsCompleted'
-  divBody.appendChild(boxReadsCompleted) //pinto reads completados en la pantalla
-
-  let boxReadsPercent = document.createElement('div')
-  boxReadsPercent.textContent = usersWithStats[i].stats.reads.percent
-  boxReadsPercent.innerHTML = boxReadsPercent.textContent + '%'
-  boxReadsPercent.className ='boxReadsPercent'
-  divBody.appendChild(boxReadsPercent) //pinto % de reads completados en la pantalla
-  //QUIZZ
-  let boxQuizesTotal = document.createElement('div')
-  boxQuizesTotal.textContent = usersWithStats[i].stats.quizes.total
-  boxQuizesTotal.innerHTML = 'Total de quizes: ' + boxQuizesTotal.textContent
-  boxQuizesTotal.className ='boxQuizesTotal'
-  divBody.appendChild(boxQuizesTotal) //pinto quizes totales en la pantalla
-
-  let boxQuizesCompleted = document.createElement('div')
-  boxQuizesCompleted.textContent = usersWithStats[i].stats.quizes.completed
-  boxQuizesCompleted.innerHTML = 'quizes completados: ' + boxQuizesCompleted.textContent
-  boxQuizesCompleted.className ='boxQuizesCompleted'
-  divBody.appendChild(boxQuizesCompleted) //pinto quizes completados en la pantalla
-
-  let boxQuizesPercent = document.createElement('div')
-  boxQuizesPercent.textContent = usersWithStats[i].stats.quizes.percent
-  boxQuizesPercent.innerHTML = boxQuizesPercent.textContent + '%'
-  boxQuizesPercent.className ='boxQuizesPercent'
-  divBody.appendChild(boxQuizesPercent) //pinto % de quizes completados en la pantalla  
-
-  let boxQuizesScoreSum = document.createElement('div')
-  boxQuizesScoreSum.textContent = usersWithStats[i].stats.quizes.scoreSum
-  boxQuizesScoreSum.innerHTML = 'Puntaje: ' + boxQuizesScoreSum.textContent + ' pts'
-  boxQuizesScoreSum.className ='boxQuizesScoreSum'
-  divBody.appendChild(boxQuizesScoreSum) //pinto scoreSum quizes completados en la pantalla
-
-  let boxQuizesScoreAvg = document.createElement('div')
-  boxQuizesScoreAvg.textContent = usersWithStats[i].stats.quizes.scoreAvg
-  boxQuizesScoreAvg.innerHTML = 'Promedio: ' + boxQuizesScoreAvg.textContent + ' pts'
-  boxQuizesScoreAvg.className = 'boxQuizesScoreAvg'
-  divBody.appendChild(boxQuizesScoreAvg) //pinto % de quizes completados en la pantalla  
-  }
+    acc[x].classList.toggle('active')
+    acc[x].nextElementSibling.style.display =
+      panelDisplay === 'block' ? 'none' : 'block'
+  })
 })
 
+// FUNCION NECESARIA PARA OCULTAR O HACER APARECER EL MENU
+let toogleSidebar = () => {
+  x = mystyle[3].style['margin-left']
+  y = mystyle[2].style['left']
 
+  mystyle[3].style['margin-left'] = x == '232px' ? '0' : '232px'
+  mystyle[2].style['left'] = y == '-232px' ? '0' : '-232px'
+}
+
+let addButtonCampuses = () => {
+  for (n in campusesRaw) {
+    if (campusesRaw[n].active === true) {
+      const namecampus = campusesRaw[n].name
+      const idcampus = campusesRaw[n].id
+      const button = document.createElement('button')
+      button.addEventListener(
+        'click',
+        function() {
+          loadCohort(idcampus)
+        },
+        false
+      )
+
+      button.className = 'submenuButton'
+      button.innerHTML = namecampus + ' ' + '<i class="fas fa-angle-right"></i>'
+
+      panel.appendChild(button)
+    }
+  }
+}
+
+let loadCohort = id => {
+
+  // REMUEVE PARA DESPEJAR DIV CONTENEDOR COHORTS
+  while (cohortGrid.firstChild) {
+    cohortGrid.removeChild(cohortGrid.firstChild)
+  }
+
+  // FILTRA LOS COHORTS POR ID QUE COINCIDA CON EL ID DEL CAMPUS
+  coursesRaw.filter(x => {
+    if (x.id.includes(id)) {
+      const firstDiv = document.createElement('div')
+      const secondDiv = document.createElement('div')
+
+      firstDiv.className = 'buttoncohort'
+      secondDiv.innerHTML = x.id
+      secondDiv.innerHTML += "<div class='subtext'> Alumnas " + x.usersCount + "</div>"
+      cohortGrid.appendChild(firstDiv)
+      firstDiv.appendChild(secondDiv)
+    }
+  })
+
+  // REVIERTE LA POSICION
+  let grid = Array.prototype.slice.call(cohortGrid.childNodes)
+  for (let i = grid.length -1; i>=0; i--) {
+    cohortGrid.appendChild(grid[i]);
+}
+}
